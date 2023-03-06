3ba2a3c998d729451f91836260ffe267dfc1ee2b
philippart-s <wildagsx@gmail.com>
2023-03-06 15:28:55 +0100

    quality: ♻️ improve code after review by @scraly

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,7 @@
 output "kubeconfig" {
-  description = "Concatened kubeconfig files"
-  value       = [for kube in ovh_cloud_project_kube.my_kube_cluster : kube.kubeconfig]
-  sensitive   = true
+  description = "Generation of each kubeconfig file"
+  count       = var.number_of_clusters
+  //content       = [for kube in ovh_cloud_project_kube.my_kube_cluster : kube.kubeconfig]
+  sensitive = true
+  filename  = "polytech-${count.index}.yml"
 }
