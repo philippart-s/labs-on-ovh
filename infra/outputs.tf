0e17fdb997c5c5e27cd47a8a105e3a618c3b4953
philippart-s <wildagsx@gmail.com>
2023-03-08 17:05:48 +0100

    clean: 🗑 remove unnecessary file

--- infra/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "kubeconfig" {
-  description = "Generation of each kubeconfig file"
-  value       = [for kube in ovh_cloud_project_kube.my_kube_cluster : kube.kubeconfig]
-  sensitive = true
-}
